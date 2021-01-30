e416f0b26d208d0a80ff5ac20a8054eff8e68b06
Joel Oredsson <joel_ored@hotmail.com>
2021-01-30 18:52:47 +0100

    Added scheduler script

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ parser.add_argument("-v", "--verbose", help="increase output verbosity",
                     action="store_true")
 parser.add_argument("-c", "--count", type=int, default=4,
                     help="Number of recipes to print.")
+parser.add_argument("-r", "--random", action="store_true",
+                    help="Sends it immediately without asking.")
 
 
 def choose_recipes(num):
@@ -37,6 +39,17 @@ def choose_recipes(num):
                 cb.print_recipe(i)
         break
 
+def send_recipes(num):
+    cb = CookBook('config.ini')
+    randRecipes = cb.get_rand_recipes_ids(num)
+
+    print("\n{} random {} from Der kleine helfer:".format(num, "recipes" if
+                                                          num > 1 else "recipe"))
+    for idx, i in enumerate(randRecipes):
+        print("{}.) {}".format(idx + 1, cb.get_recipe_title(i)))
+
+    cb.send_recipes(randRecipes)
+
 
 def main():
     args = parser.parse_args()
@@ -44,7 +57,10 @@ def main():
     if args.verbose:
         print("Running Der kleine Helfer with verbose=true, count={}".format(args.count))
 
-    choose_recipes(args.count)
+    if (args.random):
+        send_recipes(args.count)
+    else:
+        choose_recipes(args.count)
 
 
 if __name__ == "__main__":
---
+++ scheduler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import time
+import os
+
+prevDay = -1
+while True:
+    currDay = time.gmtime().tm_mday
+    currWeekDay = time.gmtime().tm_wday
+    if currWeekDay == 6 and currDay != prevDay:
+        print("Sending recipes...")
+        os.system('python3 main.py -c 4 -v -r')
+        print("Recipes sent!\n")
+        prevDay = currDay
+    time.sleep(60*5)
+
